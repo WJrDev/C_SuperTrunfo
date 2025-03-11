65efdf344e2bc49f75887d6bd8b0d0f6ad311a24
Walter Junior <waltersdjunior@gmail.com>
2025-03-11 19:56:10 -0400

    Fiz o codigo para criar as duas cartas de Super Trunfo
    A criação será dada a quem rodar o código.

--- Cartas.c
+++ Cartas.c
@@ -2,92 +2,88 @@
 
 int Main()
 {
-    #pragma region Parametros da Primeira Carta
+    //Parametros da Primeira Carta
 
-    char c1_estado;
-    char c1_codigo[3];
-    char c1_UF[2];
-    int c1_populacao;
-    float c1_area;
-    float c1_PIB;
-    int c1_numeroDePontosTuristicos;
-
-    printf("Escolha Os Parametros Da Primeira Carta!\n");
-
-    printf("Estado ");
-    scanf("%c\n", &c1_estado);
-
-    printf("Codigo ");
-    scanf("%s\n", c1_codigo);
-
-    printf("UF ");
-    scanf("%s\n", c1_UF);
-
-    printf("Populacao ");
-    scanf("%i\n", &c1_populacao);
-    
-    printf("Area ");
-    scanf("%f\n", &c1_area);
-
-    printf("PIB ");
-    scanf("%f\n", &c1_PIB);
-
-    printf("Numero de Pontos Turisticos ");
-    scanf("%i\n", &c1_numeroDePontosTuristicos);
-    #pragma endregion
-
-    #pragma region Parametros da Segunda Carta
-
-    char c2_estado;
-    char c2_codigo[3];
-    char c2_UF[2];
-    int c2_populacao;
-    float c2_area;
-    float c2_PIB;
-    int c2_numeroDePontosTuristicos;
+    char c1Estado;
+    char c1Codigo[3];
+    char c1UF[2];
+    int c1Populacao;
+    float c1Area;
+    float c1PIB;
+    int c1NumeroDePontosTuristicos;
 
     printf("Escolha Os Parametros Da Primeira Carta!\n");
 
     printf("Estado ");
-    scanf("%c\n", &c2_estado);
+    scanf("%c\n", &c1Estado);
 
     printf("Codigo ");
-    scanf("%s\n", c2_codigo);
+    scanf("%s\n", c1Codigo);
 
     printf("UF ");
-    scanf("%s\n", c2_UF);
+    scanf("%s\n", c1UF);
 
     printf("Populacao ");
-    scanf("%i\n", &c2_populacao);
+    scanf("%i\n", &c1Populacao);
     
     printf("Area ");
-    scanf("%f\n", &c2_area);
+    scanf("%f\n", &c1Area);
 
     printf("PIB ");
-    scanf("%f\n", &c2_PIB);
+    scanf("%f\n", &c1PIB);
 
     printf("Numero de Pontos Turisticos ");
-    scanf("%i\n", &c2_numeroDePontosTuristicos);
-    #pragma endregion
+    scanf("%i\n", &c1NumeroDePontosTuristicos);
 
-    #pragma region Mostrar Todas as Cartas
+    //Parametros da Segunda Carta
+
+    char c2Estado;
+    char c2Codigo[3];
+    char c2UF[2];
+    int c2Populacao;
+    float c2Area;
+    float c2PIB;
+    int c2NumeroDePontosTuristicos;
+
+    printf("Escolha Os Parametros Da Primeira Carta!\n");
+
+    printf("Estado ");
+    scanf("%c\n", &c2Estado);
+
+    printf("Codigo ");
+    scanf("%s\n", c2Codigo);
+
+    printf("UF ");
+    scanf("%s\n", c2UF);
+
+    printf("Populacao ");
+    scanf("%i\n", &c2Populacao);
+    
+    printf("Area ");
+    scanf("%f\n", &c2Area);
+
+    printf("PIB ");
+    scanf("%f\n", &c2PIB);
+
+    printf("Numero de Pontos Turisticos ");
+    scanf("%i\n", &c2NumeroDePontosTuristicos);
+
+    // Mostrar Todas as Cartas
     printf("Primaira Carta\n");
-    printf("Estado: %c\n", c1_estado);
-    printf("Codigo: %s\n", c1_codigo);
-    printf("Populacao: %i\n", c1_populacao);
-    printf("Area %.2f\n", c1_area);
-    printf("PIB %.2f\n", c1_PIB);
-    printf("Numero de Pontos Turisticos %i\n", c1_numeroDePontosTuristicos);
+    printf("Estado: %c\n", c1Estado);
+    printf("Codigo: %s\n", c1Codigo);
+    printf("Populacao: %i\n", c1Populacao);
+    printf("Area %.2f\n", c1Area);
+    printf("PIB %.2f\n", c1PIB);
+    printf("Numero de Pontos Turisticos %i\n", c1NumeroDePontosTuristicos);
 
     printf("\n");
 
     printf("Segunda Carta\n");
-    printf("Estado: %c\n", c2_estado);
-    printf("Codigo: %s\n", c2_codigo);
-    printf("Populacao: %i\n", c2_populacao);
-    printf("Area %.2f\n", c2_area);
-    printf("PIB %.2f\n", c2_PIB);
-    printf("Numero de Pontos Turisticos %i\n", c2_numeroDePontosTuristicos);
-    #pragma region 
-
+    printf("Estado: %c\n", c2Estado);
+    printf("Codigo: %s\n", c2Codigo);
+    printf("Populacao: %i\n", c2Populacao);
+    printf("Area %.2f\n", c2Area);
+    printf("PIB %.2f\n", c2PIB);
+    printf("Numero de Pontos Turisticos %i\n", c2NumeroDePontosTuristicos);
 }
